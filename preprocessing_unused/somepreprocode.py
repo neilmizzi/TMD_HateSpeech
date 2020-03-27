78616b970aa192f8457e1cf6b6a9f0ff18f6a6d4
Neil Mizzi <neilmizzi@github.com>
2020-03-27 18:22:36 +0100

    cleanup

--- preprocessing_unused/somepreprocode.py
+++
@@ -1,9 +0,0 @@
-import preprocessor as p
-import pandas as pd
-
-def clean_dataframe_with_tweets(dataframe,column_of_text):
-        dataframe.replace(':'," ", inplace = True, regex = True)
-        dataframe.replace('\t', " ", inplace=True, regex=True)
-        dataframe[column_of_text] = [p.clean(elem) for elem in dataframe[column_of_text].values]
-        dataframe[column_of_text] = [elem.strip() for elem in dataframe[column_of_text].values]
-        return dataframe
